38f1d07ee4b8853ad38890d194ce6162b88810f0
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2021-11-18 14:59:18 +0530

    first commit

---
+++ User.java
@@ -0,0 +1,35 @@
+public class User{
+    private int id;
+    private String name;
+    private long phoneNumber;
+    
+    public User(int id, String name, long phoneNumber){
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }    
+}
