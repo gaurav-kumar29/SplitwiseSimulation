c6f0ef103dc44955e104a2ae54ee8eb61868a6f1
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2021-11-23 01:09:33 +0530

    Added Manager

--- Manager.java
+++ Manager.java
@@ -9,4 +9,68 @@ public class Manager {
         userMap = new HashMap<String, User>();
         balanceSheet = new HashMap<String, Map<String, Double> >();
     }
+
+    public void addUser(User user) {
+        userMap.put(user.getId(), user);
+        balanceSheet.put(user.getId(), new HashMap<String, Double>());
+    }
+
+    public void addCredit(CreditType creditType, double amount, String paidBy, List<Split> splits) {
+        Credit credit = CreditController.createCredit(creditType, amount, userMap.get(paidBy), splits);
+        credits.add(credit);
+        for (Split split : credit.getSplits()) {
+            String paidTo = split.getUser().getId();
+            Map<String, Double> balances = balanceSheet.get(paidBy);
+            if (!balances.containsKey(paidTo)) {
+                balances.put(paidTo, 0.0);
+            }
+            balances.put(paidTo, balances.get(paidTo) + split.getAmount());
+
+            balances = balanceSheet.get(paidTo);
+            if (!balances.containsKey(paidBy)) {
+                balances.put(paidBy, 0.0);
+            }
+            balances.put(paidBy, balances.get(paidBy) - split.getAmount());
+        }
+    }
+
+    public void showBalance(String userId) {
+        boolean emptyBalance = true;
+        for (Map.Entry<String, Double> userBalance : balanceSheet.get(userId).entrySet()) {
+            if (userBalance.getValue() != 0) {
+                emptyBalance = false;
+                printBalance(userId, userBalance.getKey(), userBalance.getValue());
+            }
+        }
+
+        if (emptyBalance) {
+            System.out.println("No balances");
+        }
+    }
+
+    public void showBalances() {
+        boolean emptyBalance = true;
+        for (Map.Entry<String, Map<String, Double>> allBalances : balanceSheet.entrySet()) {
+            for (Map.Entry<String, Double> userBalance : allBalances.getValue().entrySet()) {
+                if (userBalance.getValue() > 0) {
+                    emptyBalance = false;
+                    printBalance(allBalances.getKey(), userBalance.getKey(), userBalance.getValue());
+                }
+            }
+        }
+
+        if (emptyBalance) {
+            System.out.println("No balances");
+        }
+    }
+
+    private void printBalance(String user1, String user2, double amount) {
+        String userName1 = userMap.get(user1).getName();
+        String userName2 = userMap.get(user2).getName();
+        if (amount < 0) {
+            System.out.println(userName1 + " owes " + userName2 + ": " + Math.abs(amount));
+        } else if (amount > 0) {
+            System.out.println(userName2 + " owes " + userName2 + ": " + Math.abs(amount));
+        }
+    }
 }
