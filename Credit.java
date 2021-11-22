a2a0af7943cbce757d1171d5a8d1d515e1a93188
Gaurav Kumar <gauravkumar@Gauravs-MacBook-Air.local>
2021-11-22 23:21:01 +0530

    Added Credit

---
+++ Credit.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public abstract class Credit {
+    private int id;
+    private double amount;
+    private User paidBy;
+    private List<Split> splits;    
+
+    public Credit(double amount, User paidBy, List<Split> splits) {
+        this.amount = amount;
+        this.paidBy = paidBy;
+        this.splits = splits;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public User getPaidBy() {
+        return paidBy;
+    }
+
+    public void setPaidBy(User paidBy) {
+        this.paidBy = paidBy;
+    }
+
+    public List<Split> getSplits() {
+        return splits;
+    }
+
+    public void setSplits(List<Split> splits) {
+        this.splits = splits;
+    }
+
+    public abstract boolean validate();
+}
